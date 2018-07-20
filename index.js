9ebbd585ba2292b06b02daf057d41c7c035a7607
Nicole Masarova <nicole.masarova@seasoned.co>
2018-07-20 10:35:43 -0700

    troubleshooting issues with JSON file

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 let dataResponse;
-// ATTEMPT 2
-fetch('data.json').then(response => {
-  return response.json();
-}).then(data => {
-  console.log(data);
-  dataResponse = data.issues;
-  return dataResponse;
-}).catch(err => {
-  window.alert('Sorry. An error was thrown.')
-});
 
-var str = dataResponse[0].fields.customfield_10115[0];
-var str2 = dataResponse[1].fields.customfield_10115[0];
+let datafromJSON = JSON.parse('data.json');
+let dataFromJSON1 = JSON.parse('/data.json');
+console.log(datafromJSON);
+console.log(datafromJSON1);
 
-function extractString(foo){
-	// return foo.split('name=').pop().split(',goal=').shift();
-	let subStr = foo.split('name=').pop().split(',goal=').shift();
-	console.log(subStr);
-	return subStr;
-}
+// fetch('data.json').then(response => {
+//   return response.json();
+// }).then(data => {
+//   console.log(data);
+//   dataResponse = data.issues;
+//   return dataResponse;
+// }).catch(err => {
+//   window.alert('Sorry. An error was thrown.')
+// });
 
-extractString(str);
-extractString(str2);
+// var str = dataResponse[0].fields.customfield_10115[0];
+// var str2 = dataResponse[1].fields.customfield_10115[0];
+
+// function extractString(foo){
+// 	// return foo.split('name=').pop().split(',goal=').shift();
+// 	let subStr = foo.split('name=').pop().split(',goal=').shift();
+// 	console.log(subStr);
+// 	return subStr;
+// }
+
+// extractString(str);
+// extractString(str2);
