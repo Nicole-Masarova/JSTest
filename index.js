a7e7b07be299e0bb3d15fe3af6f9ea9290d9c2e7
Nicole Masarova <nicole.masarova@seasoned.co>
2018-07-20 11:21:02 -0700

    function torubleshooting ---lots of changes

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let dataResponse;
 
-let datafromJSON = JSON.parse('data.json');
-let dataFromJSON1 = JSON.parse('/data.json');
-console.log('datafromJSON: ', datafromJSON);
-console.log('datafromJSON1: ', datafromJSON1);
+// let datafromJSON = JSON.parse('data.json');
+// let dataFromJSON1 = JSON.parse('/data.json');
+// console.log('datafromJSON: ', datafromJSON);
+// console.log('datafromJSON1: ', datafromJSON1);
 
-fetch('data.json').then(response => {
+
+function fetchJsonArray () {
+return fetch('data.json').then(response => {
   return response.json();
 }).then(data => {
   console.log('data from response: ', data);
@@ -13,17 +15,25 @@ fetch('data.json').then(response => {
   return dataResponse;
 }).catch(err => {
   window.alert('Sorry. An error was thrown.')
+});	
+}
+
+function extractString(foo){
+	// return foo.split('name=').pop().split(',goal=').shift();
+	let subStr = foo.split('name=').pop().split(',goal=').shift();
+	console.log(subStr);
+	return subStr;
+}
+
+
+let foo = fetchJsonArray().then(dataArray => {
+	console.log('[0].fields.customfield_10115[0]: ', dataArray[0].fields.customfield_10115[0]);
+	let bar = extractString(dataArray[0].fields.customfield_10115[0]);
 });
 
-// var str = dataResponse[0].fields.customfield_10115[0];
+// var str = dataResponse[0].fields.customfield_10115[0]; //this was recieving issue when the fetch wasn't a function because the promise had not finished by the time the javascript had reached this line. It was able to keep going while the promise was comopleted so DataResponse was not defined yet.
 // var str2 = dataResponse[1].fields.customfield_10115[0];
 
-// function extractString(foo){
-// 	// return foo.split('name=').pop().split(',goal=').shift();
-// 	let subStr = foo.split('name=').pop().split(',goal=').shift();
-// 	console.log(subStr);
-// 	return subStr;
-// }
 
 // extractString(str);
 // extractString(str2);
